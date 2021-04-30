378a3cee25df49b73e3e497e3fb9fc27a839a2c3
Paladinbuch <83461270+Paladinbuch@users.noreply.github.com>
2021-04-30 18:04:33 +0200

    Waschbecken platziert

---
+++ tilesets/Office_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Office_3" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="350f8b1752284d9b4a4b0e0f7aa0dff4.png" width="512" height="512"/>
+</tileset>
