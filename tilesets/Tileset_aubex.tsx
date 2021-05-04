1d69738b9d0a38e9d320ae449fef52cd3ec31ba7
Paladinbuch <83461270+Paladinbuch@users.noreply.github.com>
2021-05-04 09:24:30 +0000

    Deploying to gh-pages from master @ 4dce34058b76f7501c8bf333c3fc732b962b6f3b 🚀

--- tilesets/Tileset_aubex.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Tileset_aubex" tilewidth="32" tileheight="32" tilecount="3212" columns="44">
- <image source="Tileset_aubex.png" width="1417" height="2362"/>
-</tileset>
